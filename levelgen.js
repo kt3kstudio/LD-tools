510a66fd1671871a547a28f60fccd9dc9b49825b
Yoshiya Hinosawa <stibium121@gmail.com>
2014-12-12 00:10:42 +0900

    Create Level class

--- levelgen.js
+++ levelgen.js
@@ -1,40 +1,49 @@
 #! /usr/bin/env node
 
-var uuid = require('node-uuid');
 var argv = require('minimist')(process.argv.slice(2));
 var path = require('path');
+var Level = require('./lib/level.js');
 
 Array.prototype.random = function () {
-    return this[Math.floor(Math.random() * this.length)]
-}
+    return this[Math.floor(Math.random() * this.length)];
+};
 
-var M = argv.m;
-var N = argv.n;
+var M = 3;
+var N = parseInt(argv.n);
+var I = argv.i;
 
 var help = function () {
     var scriptName = path.basename(__filename);
 
-    console.log('Usage: %s -m num -n num', scriptName);
-    console.log('For example: %s -m 3 -n 12', scriptName);
+    console.log('Usage: %s -i id -n num', scriptName);
+    console.log('For example: %s -i 701 -n 12', scriptName);
 };
 
-if (M == null || N == null) {
+if (!N || !I) {
     help();
 
     process.exit();
-};
+}
+
 
 var level = {};
 
-level.id = uuid.v1();
-level.name = 'no name';
+level.id = I;
+level.name = I;
 
-var boms = level.boms = [];
+var kind = ['deutron', 'triton'].random();
+var number = [1, 2, 3].random();
+
+level.goals = [[kind, number]];
+
+var cells = level.cells = [];
 
 for (var i = 0; i < M; i++) {
     for (var j = 0; j < N; j++) {
-        boms.push({x: i, y: j, gender: ['f', 'm'].random()});
+        cells.push({x: i, y: j, gender: ['f', 'm'].random()});
     }
 }
 
-console.log(JSON.stringify(level, null, 2));
+var level = Level.createRandom(I, N);
+
+console.log(JSON.stringify(level.toObject(), null, 2));
